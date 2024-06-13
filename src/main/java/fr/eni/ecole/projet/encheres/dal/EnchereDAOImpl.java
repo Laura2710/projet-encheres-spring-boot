623642dc85e931dda9612e9c75f8812cbba5a61a
Laura TOMASINI <tomasinilaura1988@gmail.com>
2024-06-13 11:21:54 +0200

    correction bug recrediter acquereur

--- src/main/java/fr/eni/ecole/projet/encheres/dal/EnchereDAOImpl.java
+++ src/main/java/fr/eni/ecole/projet/encheres/dal/EnchereDAOImpl.java
@@ -16,13 +16,13 @@ import fr.eni.ecole.projet.encheres.bo.Utilisateur;
 
 @Repository
 public class EnchereDAOImpl implements EnchereDAO {
-	
+
 	private static final String FIND_MONTANT = "SELECT MONTANT_ENCHERE FROM ENCHERES WHERE id = :id";
-	private static final String TROUVER_DERNIERE_ENCHERE = "SELECT TOP 1 id_utilisateur,no_article, montant_enchere FROM encheres WHERE no_article = :idArticle ORDER BY date_enchere DESC";
+	private static final String TROUVER_DERNIERE_ENCHERE = "SELECT id_utilisateur,no_article, montant_enchere, date_enchere FROM encheres WHERE no_article=:idArticle ORDER BY date_enchere DESC";
 	private static final String AJOUTER_ENCHERE = "INSERT INTO encheres (id_utilisateur, no_article, montant_enchere) VALUES (:idUtilisateur, :idArticle, :montant)";
 	private static final String TROUVER_PAR_UTILISATEUR_ET_ARTICLE = "SELECT * FROM ENCHERES WHERE id_utilisateur=:idUtilisateur AND no_article=:idArticle";
 	private static final String UPDATE_ENCHERE = "UPDATE ENCHERES SET montant_enchere=:montant, date_enchere = GETDATE() WHERE id_utilisateur=:idUtilisateur AND no_article=:idArticle";
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -38,9 +38,9 @@ public class EnchereDAOImpl implements EnchereDAO {
 		MapSqlParameterSource param = new MapSqlParameterSource();
 		param.addValue("idArticle", id);
 		List<Enchere> result = jdbcTemplate.query(TROUVER_DERNIERE_ENCHERE, param, new EnchereRowMapper());
-	    return result.isEmpty() ? new Enchere() : result.get(0);	
+		return result.isEmpty() ? new Enchere() : result.get(0);
 	}
-	
+
 	@Override
 	public int addEnchere(Enchere enchere) {
 		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
@@ -49,23 +49,24 @@ public class EnchereDAOImpl implements EnchereDAO {
 		namedParameters.addValue("montant", enchere.getMontant());
 		return jdbcTemplate.update(AJOUTER_ENCHERE, namedParameters);
 	}
-	
+
 	public class EnchereRowMapper implements RowMapper<Enchere> {
 
 		@Override
 		public Enchere mapRow(ResultSet rs, int rowNum) throws SQLException {
 			Enchere enchere = new Enchere();
 			enchere.setMontant(rs.getInt("montant_enchere"));
-			
+		    enchere.setDateEnchere(rs.getTimestamp("date_enchere").toLocalDateTime());
+
 			Utilisateur encherisseur = new Utilisateur();
 			encherisseur.setPseudo(rs.getString("id_utilisateur"));
-			
+
 			ArticleAVendre article = new ArticleAVendre();
 			article.setId(rs.getInt("no_article"));
-			
+
 			enchere.setAcquereur(encherisseur);
 			enchere.setArticleAVendre(article);
-			
+
 			return enchere;
 		}
 
@@ -76,8 +77,8 @@ public class EnchereDAOImpl implements EnchereDAO {
 		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
 		namedParameters.addValue("idUtilisateur", pseudo);
 		namedParameters.addValue("idArticle", id);
-	    List<Enchere> result = jdbcTemplate.query(TROUVER_PAR_UTILISATEUR_ET_ARTICLE, namedParameters, new EnchereRowMapper());
-	    return result.isEmpty() ? null : result.get(0);	
+		List<Enchere> result = jdbcTemplate.query(TROUVER_PAR_UTILISATEUR_ET_ARTICLE, namedParameters,new EnchereRowMapper());
+		return result.isEmpty() ? null : result.get(0);
 	}
 
 	@Override
@@ -88,7 +89,5 @@ public class EnchereDAOImpl implements EnchereDAO {
 		namedParameters.addValue("montant", enchereExistante.getMontant());
 		jdbcTemplate.update(UPDATE_ENCHERE, namedParameters);
 	}
-
-
 
 }
